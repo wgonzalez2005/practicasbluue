27962a728cad2ccae8217b637613b3c6aee56767
Wilson Manuel Gonzalez Mejia <wgonzalez@infotep.gov.do>
2025-06-04 15:22:23 -0400

    Terminacion de la primera parte de fech

--- js/app.js
+++ js/app.js
@@ -1,123 +1,33 @@
-
-const posts = [
-    {
-        userId: 1,
-        id: 1,
-        title:
-            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body:
-            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-        userId: 1,
-        id: 2,
-        title: "qui est esse",
-        body:
-            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-        userId: 1,
-        id: 3,
-        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        body:
-            "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-    },
-];
+const url = 'https://jsonplaceholder.typicode.com/posts/';
 
 
+// const data =  fetch(url)
+//   .then((res)=>res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error('Error:', err))
+//   .finally(() => console.log('Fetch completed'));
 
-/*const findPostById = (id, callback) => {
+const FindPostById = async (id)=> {
 
-    const post = posts.find((post) => post.id === id);  
+    try{
+
+    const post = await fetch(url+id);
+    const data = await post.json();
     
-    if (post) {
-       callback(null,post);
-    }else{
-       callback("no se encontro el post con el id: " + id,null);
+    console.log(data);
+
+
+    }catch (error) {
+        console.error('Error fetching post:', error);
+        return;
     }   
+
     
-};*/
-
-/*
-findPostById(3, (err,post) => {
-    if(err){
-       return console.error(err);
-    }
-    console.log(post);
-});*/
-
-// const findPostById = (id)=>{
-
-//     const post = posts.find((post) => post.id === id);
-//     return new Promise((resolve, reject) => {
-//         if (post) {
-//             resolve(post);
-//         } else {
-//             reject("No se encontró el post con el id: " + id);
-//         }
-//     });
-// }
-
-///Promesas mas cortas
-const findPostById = id => new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-       
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        resolve(post);
-    } else {
-        reject("No se encontró el post con el id: " + id);
-    }
-    }, 2000);
     
-});
+   
 
+}
 
-
-const buscar = async () => {
-    try {
-        const post = await Promise.all([
-            findPostById(1),
-            findPostById(2)    
-        ]);
-        console.log(post[0].title+" "+post[1].title);
-    } catch (err) {
-        console.error(err);
-    } finally{
-        
-        console.log("Finalizando la busqueda");
-    }
-};
-
-buscar();
-// const buscar = async (id) => {
-//     try {
-//         const post = await findPostById(id);
-//         console.log(post);
-//     } catch (err) {
-//         console.error(err);
-//     } finally{
-        
-//         console.log("Finalizando la busqueda");
-//     }
-// };
-
-// buscar(3);
-
-
-// findPostById(1)
-//     .then((post) =>console.log(post))
-//     .catch((err) => console.error(err));
-
-
-    console.log("Termionacion del programa");
-
-
-
-
-
-
-
+FindPostById(75);
 
 
