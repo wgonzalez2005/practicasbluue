ee19058354d547851ef5fd1868a2b2c5eecc2413
Wilson Manuel Gonzalez Mejia <wgonzalez@infotep.gov.do>
2025-06-04 12:34:10 -0400

    Terminacion de studios de las promesas

--- js/app.js
+++ js/app.js
@@ -1,106 +1,117 @@
 
-const formulario = document.getElementById("formulario");
-const userName = document.getElementById("userName");
-const userEmail = document.getElementById("userEmail");
-
-
-const alertName = document.getElementById("alertName");
-const alertEmail = document.getElementById("alertEmail");
-const alertSuccess = document.getElementById("alertSuccess");
-
-const regexEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
-const soloLetras = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-
-
-const pintarAlerta = () => {
-console.log("pintarAlerta");
- alertSuccess.classList.remove("d-none");
- alertSuccess.textContent = "Formulario enviado correctamente";
-
-};
-
-
-const pintarMensajeError = (errores) => {
-
-errores.forEach((item) => {
-
-    item.tipo.classList.remove("d-none");   
-    item.tipo.textContent = item.msg;
-
-
-});
-
-};
+const posts = [
+    {
+        userId: 1,
+        id: 1,
+        title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body:
+            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "qui est esse",
+        body:
+            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    },
+    {
+        userId: 1,
+        id: 3,
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        body:
+            "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    },
+];
 
 
 
-formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const errores = [];
+/*const findPostById = (id, callback) => {
 
-
-     alertSuccess.classList.add("d-none");
-
-    // Validar nombre
-    if (!userName.value.trim() || !soloLetras.test(userName.value)) {
-
-        userName.classList.add("is-invalid", "alert-danger");
-
-        errores.push({
-            tipo:alertName,
-            msg:'Formato no valido en el campo nombre, solo letras'
-         });
-
-    } else{
- 
-        
-        
-        userName.classList.add("is-valid");       
-        userName.classList.remove("is-invalid", "alert-danger");
-        alertName.classList.add("d-none")
-        console.log(alertName);
-    }
-
-    // Validar email
-    if (!userEmail.value.trim() || !regexEmail.test(userEmail.value)) {
-       
-       userEmail.classList.add("is-invalid", "alert-danger");
-
-       errores.push({
-            tipo:alertEmail,
-            msg:'Escriba un correo valido'
-         });
-
-    }else{
-        
-        
-        alertEmail.classList.add("d-none");
-        userEmail.classList.add("is-valid");       
-        userEmail.classList.remove("is-invalid", "alert-danger");
-  
-    }
-
-
-    if(errores.length!==0){
-            pintarMensajeError(errores);
-            return
-
-    }
-
-    pintarAlerta();
-
-    const formData = new FormData(formulario);
+    const post = posts.find((post) => post.id === id);  
     
-    for(let item of formData){
-        console.log(item);
+    if (post) {
+       callback(null,post);
+    }else{
+       callback("no se encontro el post con el id: " + id,null);
+    }   
+    
+};*/
+
+/*
+findPostById(3, (err,post) => {
+    if(err){
+       return console.error(err);
     }
+    console.log(post);
+});*/
 
+// const findPostById = (id)=>{
 
+//     const post = posts.find((post) => post.id === id);
+//     return new Promise((resolve, reject) => {
+//         if (post) {
+//             resolve(post);
+//         } else {
+//             reject("No se encontró el post con el id: " + id);
+//         }
+//     });
+// }
 
+///Promesas mas cortas
+const findPostById = id => new Promise((resolve, reject) => {
 
-
-   
+    setTimeout(() => {
+       
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        resolve(post);
+    } else {
+        reject("No se encontró el post con el id: " + id);
+    }
+    }, 2000);
+    
 });
+
+
+
+const buscar = async () => {
+    try {
+        const post = await Promise.all([
+            findPostById(1),
+            findPostById(2)    
+        ]);
+        console.log(post[0].title+" "+post[1].title);
+    } catch (err) {
+        console.error(err);
+    } finally{
+        
+        console.log("Finalizando la busqueda");
+    }
+};
+
+buscar();
+// const buscar = async (id) => {
+//     try {
+//         const post = await findPostById(id);
+//         console.log(post);
+//     } catch (err) {
+//         console.error(err);
+//     } finally{
+        
+//         console.log("Finalizando la busqueda");
+//     }
+// };
+
+// buscar(3);
+
+
+// findPostById(1)
+//     .then((post) =>console.log(post))
+//     .catch((err) => console.error(err));
+
+
+    console.log("Termionacion del programa");
 
 
 
